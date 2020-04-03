cf35d26cdea3681007ec366d81e5145cb44f7caf
r.nishino <r.nishino@stonesystem.co.jp>
2020-04-03 15:35:54 +0900

    [add] マウスドラッグによる描画機能の実装

---
+++ src/views/Paint.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <canvas ref="paintArea" class="paintArea" @mousemove="draw" @mousedown="drawStart" @mouseup="drawEnd" width="400" height="400"></canvas>
+    <p>
+    {{ msg }}
+    </p>
+  </div>
+</template>
+
+<script>
+// マウスカーソルの座標
+var x = 0;
+var y = 0;
+
+// ドラッグしているか
+var isDrag = false;
+
+export default {
+  data() {
+    return {
+      msg: 'Paint Area'
+    }
+  },
+  methods: {
+    draw: function() {
+      // dragしていない時は描画しない
+      if(!isDrag) {
+        return
+      }
+      console.log('draw')
+
+      // 要素取得
+      const element = this.$refs.paintArea
+      const context = element.getContext('2d')
+
+      // canvasの座標取得
+      const canvasPosX = element.getBoundingClientRect().left
+      const canvasPosY = element.getBoundingClientRect().top
+
+      // canvas内でのマウスカーソルの座標取得
+      x = event.pageX - canvasPosX
+      y = event.pageY - canvasPosY
+      // console.log(x)
+      // console.log(y)
+
+      // TODO: 描画設定
+      context.strokeStyle = 'black'
+      context.lineWidth = '10'
+      context.lineCap = 'round'
+
+      // 描画処理
+      context.beginPath()
+      context.moveTo(x, y)
+      context.lineTo(x, y)
+      context.stroke()
+    },
+    drawStart: function() {
+      // console.log('start')
+      isDrag = true
+    },
+    drawEnd: function() {
+      // console.log('end')
+      isDrag = false
+    }
+  }
+}
+</script>
+
+<style>
+.paintArea {
+  border: solid 3px #000000;
+}
+</style>
